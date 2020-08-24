06147c83353ad7cf9e50d4afd73cb8450266457f
Saraah Bikai <supaerv@gmail.com>
2020-08-24 18:16:27 -0100

    exo

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+    @answer
+  end
+  def answer
+    @question = params[:answer]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don’t care, get dressed and go to work!"
+    end
+  end
+end
